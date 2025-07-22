493c41f8b0f6220a5168fa85efff0a448106a608
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-22 17:54:47 +0300

    initial - Create a pages folder to test a routes

--- app/app.vue
+++ app/app.vue
@@ -1,22 +1,9 @@
 <script setup lang="ts">
-const hello = ref('hello world');
-const number = ref<number>(100);
-
-const runtime = useRuntimeConfig();
-const token = ref(runtime.token);
-
-// console.log(hello.value);
 
 </script>
 
 <template>
   <div>
-    {{ hello }}
-    <!-- <TestOne /> -->
-
-    {{ token }}
-    {{ number }}
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   runtimeConfig: {
-    token: '123435435',
+    token: '',
+    apiUrl: '',
   },
 
   modules: [
