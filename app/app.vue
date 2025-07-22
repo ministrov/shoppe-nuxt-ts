851004eee09061bea6890a8c3ba2eea62d8f8036
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-22 15:42:50 +0300

    initial - Create a test component

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+const hello = ref('hello world');
+
+console.log(hello.value);
+
+</script>
+
 <template>
   <div>
+    {{ hello }}
+    <TestComponent />
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
