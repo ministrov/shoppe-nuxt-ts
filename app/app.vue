c0c53cc2167ddf2ac2bcb03f201f6e6bca31bd36
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-22 17:30:21 +0300

    initial - Creating a nuxt config runtime const to store info

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 const hello = ref('hello world');
+const number = ref<number>(100);
+
+const runtime = useRuntimeConfig();
+const token = ref(runtime.token);
 
 // console.log(hello.value);
 
@@ -8,7 +12,10 @@ const hello = ref('hello world');
 <template>
   <div>
     {{ hello }}
-    <TestOne />
+    <!-- <TestOne /> -->
+
+    {{ token }}
+    {{ number }}
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  runtimeConfig: {
+    token: '123435435',
+  },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
