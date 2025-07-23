eed368a71c379ca8fe7b89d01b86f344192e1d5b
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-23 10:46:52 +0300

    feature - Create an error page to test a page not found case

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,13 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+throw createError({
+  statusCode: 401,
+  statusMessage: "You are not authorized",
+  data: {
+    userId: 123
+  }
+});
+</script>
---
+++ app/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ error.statusCode }}</h1>
+
+    <div>{{ error.statusMessage }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+const { error } = defineProps<{
+  error: NuxtError
+}>();
+</script>
