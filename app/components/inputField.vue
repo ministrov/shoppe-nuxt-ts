4683241bc50b2edafb4506e99fce925ab1030bb7
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-23 18:38:14 +0300

    feature - Start creating an inputField component

---
+++ app/components/inputField.vue
@@ -0,0 +1,33 @@
+<template>
+  <input v-model="data" class="input" :class="variant" />
+</template>
+
+<script setup lang="ts">
+const { variant } = defineProps<{
+  variant: "gray" | "black";
+}>();
+const data = defineModel<string>();
+
+console.log(data);
+</script>
+
+<style scoped>
+.input {
+  width: 100%;
+  padding: 12px 0;
+  background: none;
+  border: none;
+  color: var(--color-black);
+  font-size: 16px;
+  font-weight: 400;
+}
+.input::placeholder {
+  color: var(--color-dark-gray);
+}
+.gray {
+  border-bottom: 1px solid var(--color-gray);
+}
+.black {
+  border-bottom: 1px solid var(--color-black);
+}
+</style>
