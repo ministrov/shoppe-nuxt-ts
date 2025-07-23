9adf3892bfb06ebbe5facd0a9a4e1b3b7e44c5bd
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-23 10:29:06 +0300

    feature - Testing a programming navigation with navigateTo functinon in nuxt

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,13 +3,22 @@
   <div class="home">
     <h1>Welcome to the Home Page</h1>
     <p>This is your first Nuxt page.</p>
+
+    <NuxtLink to="/about">About</NuxtLink>
+
+    <!-- <button @click="openAbout">Move to</button> -->
   </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+// function openAbout() {
+//   navigateTo("/about");
+// }
+</script>
 
 <style scoped>
 .home {
+  padding: 2rem;
   background-color: rebeccapurple;
 }
 </style>
