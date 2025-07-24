1d8605441e84cfc37745ee0b65d98ffc8dc7b81b
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-24 18:55:21 +0300

    feature - End up operating with layouts

--- app/components/LayoutHeader.vue
+++ app/components/LayoutHeader.vue
@@ -28,6 +28,14 @@
   </div>
 </template>
 
+<script setup>
+console.log("Setup");
+
+onMounted(() => {
+  console.log("Mounted");
+});
+</script>
+
 <style scoped>
 .header {
   display: flex;
@@ -35,6 +43,58 @@
   margin-top: 48px;
   max-width: 1248px;
   margin: 0 auto;
+  margin-top: 48px;
   padding: 0 16px;
+  border-bottom: 1px solid var(--color-gray);
+}
+
+.header__content {
+  display: flex;
+  align-items: center;
+  gap: 60px;
+}
+
+.header__menu {
+  display: flex;
+  gap: 60px;
+  align-items: center;
+}
+
+.header__menu a {
+  padding-bottom: 48px;
+  font-size: 16px;
+  line-height: 27px;
+  color: var(--color-black);
+  text-decoration: none;
+}
+
+.header__menu a:hover {
+  color: var(--color-dark-gray);
+}
+
+.header__menu a.router-link-active {
+  border-bottom: 2px solid var(--color-black);
+}
+
+.header__icons {
+  display: flex;
+  align-items: center;
+  gap: 40px;
+}
+
+.header__icons a {
+  border-bottom: none;
+  padding-bottom: 48px;
+}
+
+.header__icons a.router-link-active {
+  border-bottom: 2px solid var(--color-black);
+}
+
+.header__hr {
+  align-self: flex-start;
+  height: 17px;
+  margin-top: 4px;
+  border-left: 1px solid var(--color-dark-gray);
 }
 </style>
