429c942cc2de29e64690d56f767654cc9109b25f
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-24 13:34:49 +0300

    feature - End up operating with a LayoutFooter and start creating a LayoutHeader

---
+++ app/components/LayoutHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="header">
+    <IconLogo />
+  </div>
+</template>
+
+<style scoped></style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header>HEader</header>
+    <LayoutHeader />
     <slot />
     <LayoutFooter />
   </div>
