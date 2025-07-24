6d178b56dadf06cf576017d2dd670f4e7ee06a98
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-24 13:52:06 +0300

    feature - Add a icons svg for LayoutHeader

--- app/components/LayoutHeader.vue
+++ app/components/LayoutHeader.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="header">
-    <IconLogo />
+    <NuxtLink to="/">
+      <IconLogo />
+    </NuxtLink>
+
+    <div>MENU</div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 48px;
+  max-width: 1248px;
+  margin: 0 auto;
+  padding: 0 16px;
+}
+</style>
