4eb99fb8b874448b78c32ff018325583c9c3696c
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-29 12:17:47 +0300

    feature - Write a request to fetch a categories data and create interface for category, for response

--- app/pages/catalog/index.vue
+++ app/pages/catalog/index.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <h1>Welcome to the Catalog Page</h1>
+    <!-- <h1>Welcome to the Catalog Page</h1> -->
   </div>
 </template>
 
 <script setup lang="ts">
-const config = useRuntimeConfig();
+import type { GetCategoriesResponse } from "~/interfaces/category.interface";
 
-console.log(config);
+const config = useRuntimeConfig();
+const API_URL = config.public.apiurl;
+const data = await $fetch<GetCategoriesResponse>(API_URL + "/categories");
+
+console.log(data);
+// console.log(config.public);
 </script>
