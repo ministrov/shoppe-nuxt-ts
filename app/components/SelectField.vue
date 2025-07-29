045274d26a4fa75ab87c67bcf0cdc353330242a8
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-29 18:20:35 +0300

    feature - Start creating a selectField componet, add a basic structure

---
+++ app/components/SelectField.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="seletc-wrapper">
+    <select v-model="model" class="select">
+      <option v-for="item in options" :key="item.value" :value="item.value">
+        {{ item.label }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script setup lang="ts">
+const model = defineModel<string>();
+const { options } = defineProps<{
+  options: { label: string; value: string }[];
+}>();
+</script>
