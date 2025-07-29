d2d2855a0fbc90840ed54b11aa441231f4d74de8
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-29 18:39:14 +0300

    feature - Add a styles to selectField component

--- app/pages/catalog/index.vue
+++ app/pages/catalog/index.vue
@@ -1,27 +1,33 @@
 <template>
   <div>
     <h1>Welcome to the Catalog Page</h1>
-    <!-- <h1>Welcome to the Catalog Page</h1> -->
-    <InputField v-model="input" variant="black" />
-    <!-- <input v-model="input" type="text" /> -->
-    <button @click="sendData">Send</button>
+
+    <SelectField
+      v-model="select"
+      :options="[
+        { value: '', label: 'Категории' },
+        { value: 'option1', label: 'Option 1' },
+        { value: 'option2', label: 'Option 2' },
+      ]"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import type { GetCategoriesResponse } from "~/interfaces/category.interface";
+// import type { GetCategoriesResponse } from "~/interfaces/category.interface";
 
 const input = ref("");
-const API_URL = "http://localhost:3000/api";
+const select = ref("");
+// const API_URL = "http://localhost:3000/api";
 
-async function sendData() {
-  const data = await $fetch<GetCategoriesResponse>(API_URL + "/categories", {
-    method: "POST",
-    body: input,
-  });
+// async function sendData() {
+//   const data = await $fetch<GetCategoriesResponse>(API_URL + "/categories", {
+//     method: "POST",
+//     body: input,
+//   });
 
-  console.log(data);
-}
+//   console.log(data);
+// }
 
 console.log(input.value);
 </script>
