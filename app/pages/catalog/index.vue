3a18a4ddcf8f19a74aa1a1fbc7f4fef0a9859fb1
ministrov <61511030+ministrov@users.noreply.github.com>
2025-07-30 15:10:30 +0300

    feature - Get an array of categories from api and transform to categoriesSelect object

--- app/pages/catalog/index.vue
+++ app/pages/catalog/index.vue
@@ -2,54 +2,39 @@
   <div>
     <h1 class="left">Каталог товаров</h1>
 
-    <SelectField
-      v-model="select"
-      :options="[
-        { value: '', label: 'Категории' },
-        { value: 'option1', label: 'Option 1' },
-        { value: 'option2', label: 'Option 2' },
-      ]"
-    />
-
-    <!-- <button @click="makeRequest">Send</button> -->
+    <div class="catalog">
+      <div class="catalog__filter">
+        <SelectField v-model="select" :options="categoriesSelect" />
+      </div>
+      <div class="catalog__cards"></div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-// import type { GetCategoriesResponse } from "~/interfaces/category.interface";
+import type { GetCategoriesResponse } from "~/interfaces/category.interface";
 
-const input = ref("");
+const API_URL = "http://localhost:3000/api";
+// const input = ref("");
 const select = ref("");
-// const API_URL = "http://localhost:3000/api";
-// async function* createAsyncIterable() {
-//   yield 1;
-//   yield 2;
-//   yield 3;
-// }
-// async function makeRequest() {
-//   try {
-//     const data = await $fetch<GetCategoriesResponse>(API_URL + "/categories");
-//     console.log(data, "data");
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// async function main() {
-//   for await (const item of createAsyncIterable()) {
-//     console.log(`The current item is ${item} and the type is ${typeof item}`);
-//   }
-// }
-
-// main();
-// async function sendData() {
-//   const data = await $fetch<GetCategoriesResponse>(API_URL + "/categories", {
-//     method: "POST",
-//     body: input,
-//   });
-
-//   console.log(data);
-// }
-
-console.log(input.value);
+const { data } = await useFetch<GetCategoriesResponse>(API_URL + "/categories");
+const categoriesSelect = computed(() => {
+  return data.value
+    ? data.value?.categories.map((category) => ({
+        value: category.id.toString(),
+        label: category.name,
+      }))
+    : [];
+});
 </script>
+
+<style scoped>
+.catalog {
+  display: flex;
+  gap: 36px;
+}
+
+.catalog__filter {
+  width: 260px;
+}
+</style>
