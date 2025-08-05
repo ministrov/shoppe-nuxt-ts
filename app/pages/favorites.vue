21ea8c80aa6d93c3a1974b03c43ebeeee6941145
ministrov <61511030+ministrov@users.noreply.github.com>
2025-08-05 11:28:53 +0300

    feature - Create a favorites to fetch data and render a list of cards

--- app/pages/favorites.vue
+++ app/pages/favorites.vue
@@ -1,14 +1,39 @@
 <template>
-  <div>
-    <h1>Welcome to Favorites Page</h1>
+  <div class="catalog__grid">
+    <h1 class="left">Избранное</h1>
+    <CatalogCard
+      v-for="product in products"
+      :key="product.id"
+      v-bind="product"
+    />
   </div>
 </template>
 
-<script setup land="ts">
-// const favoriteStore = useFavoritesStore();
-// favoriteStore.addToFavorites(1);
-// await favoriteStore.fetchFavorites();
-// await callOnce("fetchFavorites", async () => favoriteStore.fetchFavorites(), {
-//   mode: "navigation",
-// });
+<script setup lang="ts">
+import type { Product } from "~/interfaces/product.interface";
+const favoriteStore = useFavoritesStore();
+const config = useRuntimeConfig();
+const API_URL = config.public.apiurl;
+const products = ref<Product[]>();
+
+console.log(products);
+console.log(products.value);
+
+watchEffect(async () => {
+  const data = await Promise.all(
+    favoriteStore.favoriteIds.map((id) => {
+      return $fetch<{ product: Product }>(API_URL + "/products/" + id);
+    })
+  );
+  products.value = data.map((el) => el.product);
+});
 </script>
+
+<style scoped>
+.catalog__grid {
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 64px 12px;
+}
+</style>
