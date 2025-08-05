1b6733f6876811b78fe15349c3001e7ae68ffd09
ministrov <61511030+ministrov@users.noreply.github.com>
2025-08-05 14:05:12 +0300

    feature - End up operating on pinia state and favorites page and start working on about page, do basic markup

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,11 +1,54 @@
 <template>
-  <section class="about-page">
-    <h1>О нас</h1>
+  <section class="about">
+    <h1 class="about__title">О нас</h1>
+
+    <h2 class="about__subtitle">Мы делаем шикарные украшения для вас</h2>
+
+    <p class="about__text">
+      Duis rutrum dictum libero quis rutrum. Etiam sed neque aliquam,
+      sollicitudin ante a, gravida arcu. Nam fringilla molestie velit, eget
+      pellentesque risus scelerisque a. Nam ac urna maximus, tempor magna et,
+      placerat urna. Curabitur eu magna enim. Proin placerat tortor lacus, ac
+      sodales lectus placerat quis.
+    </p>
+
+    <ul class="about__list">
+      <li class="about__item">
+        <article class="about__item-content">
+          <h3>Тренды в украшениях</h3>
+          <img src="" alt="" />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            placerat, augue a volutpat hendrerit, sapien tortor faucibus augue,
+            a maximus elit ex vitae libero. Sed quis mauris eget arcu facilisis
+            consequat sed eu felis.
+          </p>
+
+          <ul>
+            <li>consectetur adipiscing elit. Aliquam placerat</li>
+            <li>Lorem ipsum dolor sit amet consectetur</li>
+          </ul>
+        </article>
+      </li>
+      <li class="about__item">
+        <article class="about__item-content">
+          <h3>Сделано с любовью</h3>
+          <img src="" alt="" />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+            placerat, augue a volutpat hendrerit, sapien tortor faucibus augue,
+            a maximus elit ex vitae libero. Sed quis mauris eget arcu facilisis
+            consequat sed eu felis. Nunc sed porta augue. Morbi porta tempor
+            odio, in molestie diam bibendu.ss
+          </p>
+        </article>
+      </li>
+    </ul>
   </section>
 </template>
 
 <style>
-.about-page {
+.about {
   min-height: 100vh;
 }
 </style>
