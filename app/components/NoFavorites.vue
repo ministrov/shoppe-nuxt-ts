0068fb96143a9209ef2ad35c18f33da44e8a35e4
ministrov <61511030+ministrov@users.noreply.github.com>
2025-08-05 19:24:27 +0300

    feature - Create a component to show a user that it is no favorites added yet

---
+++ app/components/NoFavorites.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="no-favorites">
+    <h1>You have not added a favorites yet!!!</h1>
+  </div>
+</template>
+
+<style scoped>
+.no-favorites {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 50vh;
+}
+</style>
--- app/pages/favorites.vue
+++ app/pages/favorites.vue
@@ -2,6 +2,8 @@
   <div class="catalog">
     <h1 class="left">Избранное</h1>
 
+    <NoFavorites v-if="products?.length === 0" />
+
     <ul class="catalog__grid">
       <li class="catalog__item">
         <CatalogCard
@@ -39,11 +41,15 @@ watchEffect(async () => {
   display: flex;
   flex-direction: column;
   gap: 40px;
+  min-height: 100vh;
 }
 .catalog__grid {
   display: grid;
   width: 100%;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   gap: 64px 12px;
+  margin: 0;
+  padding: 0;
+  list-style: none;
 }
 </style>
